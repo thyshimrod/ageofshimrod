679bb33e8142e144ee76284a7b2f81c5728fe81c
thyshimrod <thyshimrod@gmail.com>
2021-08-13 22:01:33 +0200

    map herbe

--- src/game.js
+++ src/game.js
@@ -2,25 +2,26 @@
 var ageofshimrod = ageofshimrod || {};
 
 ageofshimrod.GameEngine = function (){
-
 }
 
 ageofshimrod.GameEngine.prototype ={
   gameLoop: function (){
     ageofshimrod.canvas.clearCanvas();
+    ageofshimrod.map.render();
   },
 
   clickEvent : function(evt){
     
   },
 
-
-
   init : function(){
     ageofshimrod.canvas = new ageofshimrod.Canvas();
     ageofshimrod.canvas.init();
     ageofshimrod.canvas.setCanvasSize(window.innerWidth,window.innerHeight);
     ageofshimrod.canvas.canvasMouse.addEventListener("click",ageofshimrod.gameEngine.clickEvent);
+    ageofshimrod.tileset = new ageofshimrod.Tileset();
+    ageofshimrod.map = new ageofshimrod.Map();
+    ageofshimrod.map.init();
   },
 }
 
---
+++ src/map.js
@@ -0,0 +1,37 @@
+'use strict';
+var ageofshimrod = ageofshimrod || {};
+
+ageofshimrod.Map = function (){
+    this.sizeX = 300;
+    this.sizeY = 300;
+    this.tileset = "./assets/tileset/tileset1.png";
+    this.tileGrassX = 192;
+    this.tileGrassY = 64;
+    this.spriteset = undefined;
+    this.ctx = undefined;
+}
+
+ageofshimrod.Map.prototype ={
+    init : function(){
+        this.spriteset = ageofshimrod.tileset.get(this.tileset);
+    },
+
+    render : function(){
+        this.ctx = ageofshimrod.canvas.canvasTile.getContext("2d");
+        for (let i = 0 ; i < this.sizeX ; i++){
+            for (let j = 0 ; j < this.sizeY ; j++){
+                this.ctx.drawImage(
+                    this.spriteset,
+                    this.tileGrassX,
+                    this.tileGrassY,
+                    32,
+                    32,
+                    i*32,
+                    j*32,
+                    32,
+                    32);
+             
+            }
+        }
+    }
+}
