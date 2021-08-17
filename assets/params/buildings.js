93bfea567a154d418b8fd49986941667fdd630b9
thyshimrod <thyshimrod@gmail.com>
2021-08-17 21:04:28 +0200

    maison de mineur

--- assets/params/buildings.js
+++ assets/params/buildings.js
@@ -33,5 +33,23 @@ ageofshimrod.Buildings = [
             }
         ],
         "ressource" : ageofshimrod.C.RESSOURCE_BOIS
+    },
+    {
+        "name"    : "Mineur",
+        "size":{"x" : 92, "y" : 106},
+        "tileset" : "assets/tileset/pngwing.com.png",
+        "sprite" : { "x" : 942, "y" : 438},
+        "capacity" : 2,
+        "materiaux" : [
+            {
+                "id" : 2,
+                "quantity" : 100
+            },
+            {
+                "id" : 1,
+                "quantity" : 100
+            }
+        ],
+        "ressource" : ageofshimrod.C.RESSOURCE_PIERRE
     }
 ]
