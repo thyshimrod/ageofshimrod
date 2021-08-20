6c314924f4209b278ab641fd854b9fa37368aa58
thyshimrod <thyshimrod@gmail.com>
2021-08-20 23:17:24 +0200

    affichage d'une jauge de vie

--- src/peon.js
+++ src/peon.js
@@ -19,6 +19,8 @@ ageofshimrod.Peon = function (){
     this.ressource = {};
     this.animationTick = 0;
     this.collectTick = 0;
+    this.hp = 5;
+    this.hpMax = 10;
 }
 
 ageofshimrod.Peon.prototype ={
@@ -202,17 +204,32 @@ ageofshimrod.Peon.prototype ={
     renderCollect : function(){
         this.ctx.beginPath();
         this.ctx.fillStyle = ageofshimrod.C.UI_RECT_COLOR;
-        this.ctx.fillRect(this.x+5,this.y-11,30,10);
+        this.ctx.fillRect(this.x+5,this.y-15,30,10);
         this.ctx.beginPath();
         this.ctx.strokeStyle = ageofshimrod.C.UI_BORDER_COLOR;
-        this.ctx.rect(this.x+5,this.y-11,30,10);
+        this.ctx.rect(this.x+5,this.y-15,30,10);
         this.ctx.stroke();
         this.ctx.font = "10px Verdana";
         this.ctx.fillStyle = ageofshimrod.C.UI_FONT_COLOR;
         let text = typeof this.ressource.quantity !== "undefined" ? this.ressource.quantity : 0;
         this.ctx.fillText(text ,
             this.x + 14, 
-            this.y - 3);
+            this.y - 7);
+    },
+
+    renderJaugeHp : function(){
+        var prctLife = Math.round((this.hp/this.hpMax)*30);
+        if (prctLife < 0) prctLife = 0;
+        this.ctx.beginPath();
+        this.ctx.fillStyle = ageofshimrod.C.UI_RECT_COLOR;
+        this.ctx.fillRect(this.x+5,this.y-5,30,3);
+        this.ctx.beginPath();
+        this.ctx.strokeStyle = ageofshimrod.C.UI_BORDER_COLOR;
+        this.ctx.rect(this.x+5,this.y-5,30,3);
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.fillStyle = ageofshimrod.C.UI_BORDER_RED;
+        this.ctx.fillRect(this.x+5,this.y-5,prctLife,3);
     },
 
     renderPosition : function(x,y,ctx){
@@ -232,8 +249,8 @@ ageofshimrod.Peon.prototype ={
     render : function(){
         this.renderPosition(this.x,this.y,this.ctx);
         if (typeof this.affectation !== "undefined" && this.affectation.name !== "Maison"){
-            this.renderCollect();
+           this.renderCollect();
         }
-        
+        this.renderJaugeHp();
     },
 }
